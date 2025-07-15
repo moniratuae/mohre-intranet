f604b2e2737575da05c69f6160208f6f1a4fb968
moniratuae <moniratuae@gmail.com>
2025-07-15 12:47:40 +0400

    Add files via upload

---
+++ mohre-intranet/scripts/customjs.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 }
+      }
+    });
+  });
